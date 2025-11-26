42a942e466bee43787fec396fbdbea2a48a3e101
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-26 20:54:15 +0600

    unWanted imports removed

--- server/src/main/java/com/pms/monolith/dto/UserRequestDTO.java
+++ server/src/main/java/com/pms/monolith/dto/UserRequestDTO.java
@@ -2,13 +2,10 @@ package com.pms.monolith.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Date;
 
 @Getter
 @Setter
