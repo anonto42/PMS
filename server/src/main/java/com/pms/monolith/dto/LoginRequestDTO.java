8f72d88b0ad22648f410be7a81ad0bcdde2d02cf
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-26 20:43:09 +0600

    Login RequestDto created

---
+++ server/src/main/java/com/pms/monolith/dto/LoginRequestDTO.java
@@ -0,0 +1,20 @@
+package com.pms.monolith.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Email is required!")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required!")
+    @Size(min = 6, max = 50, message = "Password must be at least 6 characters!")
+    private String password;
+}
