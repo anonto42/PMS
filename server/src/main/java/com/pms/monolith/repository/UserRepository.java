5bb5c58ff3d8426c78fa4ce08f97539632f30e02
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-26 20:00:40 +0600

    user and prescription repository's added

---
+++ server/src/main/java/com/pms/monolith/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.pms.monolith.repository;
+
+import com.pms.monolith.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
